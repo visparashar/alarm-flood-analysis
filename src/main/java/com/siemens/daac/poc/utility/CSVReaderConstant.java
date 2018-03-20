bc4fac6d77610185d98bc78cc6268ac60be5227e
z003w4je <z003w4je@AAEINGGN00535D.ad001.siemens.net>
2018-03-20 13:01:46 +0530

    Changes for POC

--- src/main/java/com/siemens/daac/poc/utility/CSVReaderConstant.java
+++ src/main/java/com/siemens/daac/poc/utility/CSVReaderConstant.java
@@ -2,8 +2,13 @@ package com.siemens.daac.poc.utility;
 
 public class CSVReaderConstant {
 public static final String FLOOD_STATUS = "floodStatus";
+public static final String FLOOD_STATUS_CSV = "Flood Status";
 public static final String PERCENTAGE_OF_TRUE_FLOOD = "percentageOfTrueFlood";
 public static final String TRUE = "true";
 public static final String FALSE = "false";
 public static final String NOT_SURE = "notSure";
+public static final String COMMA = ",";
+public static final String CSV_EXTENSION=".csv";
+public static final String FILE_SEP="_1";
+public static final String LINE_SEPARATOR = "line.separator";
 }
--- src/main/java/com/siemens/daac/poc/utility/CSVReaderUtil.java
+++ src/main/java/com/siemens/daac/poc/utility/CSVReaderUtil.java
@@ -1,11 +1,14 @@
 package com.siemens.daac.poc.utility;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -44,6 +47,7 @@ public class CSVReaderUtil {
 			}
 			statusPercentageMap.put(CSVReaderConstant.FLOOD_STATUS, floodStatus);
 			statusPercentageMap.put(CSVReaderConstant.PERCENTAGE_OF_TRUE_FLOOD, String.valueOf(percentageOfTrueFlood));
+			addColumn(inputFilePath.replace(".csv", ""),floodStatus);
 			count = 0;
 		} catch (IOException e) {
 
@@ -56,4 +60,37 @@ public class CSVReaderUtil {
 		count++;
 		return p[4] + p[5];
 	};
+
+	private static void addColumn(String fileName, String floodStatus) throws IOException {
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		final String lineSep = System.getProperty(CSVReaderConstant.LINE_SEPARATOR);
+
+		try {
+			File file = new File(fileName + CSVReaderConstant.CSV_EXTENSION);
+			File file2 = new File(fileName + CSVReaderConstant.FILE_SEP + CSVReaderConstant.CSV_EXTENSION);// so the
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file2)));
+			String line = null;
+			int i = 0;
+			for (line = br.readLine(); line != null; line = br.readLine(), i++) {
+				if (i==0) {
+					String addedColumn = CSVReaderConstant.COMMA + CSVReaderConstant.FLOOD_STATUS_CSV;
+					bw.write(line + addedColumn + lineSep);
+				} else {
+				String addedColumn = String.valueOf(CSVReaderConstant.COMMA + floodStatus);
+				bw.write(line + addedColumn + lineSep);
+				}
+			}
+
+		} catch (Exception e) {
+			
+		} finally {
+			if (br != null)
+				br.close();
+			if (bw != null)
+				bw.close();
+		}
+
+	}
 }
