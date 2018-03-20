a4b669194aa01220f639d556757e8532d22c6a1c
Vishal <vis.parashar@gmail.com>
2018-03-20 10:34:20 +0530

    first commit

---
+++ src/main/java/com/siemens/daac/poc/AlarmFoodAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.siemens.daac.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmFoodAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmFoodAnalysisApplication.class, args);
+	}
+}
