289b961b8aa2bb135cf9e801df9043def34beeb5
Z003WFKY <Z003WFKY@AAEINGGN01896D.ad001.siemens.net>
2018-04-02 10:09:18 +0530

    Prefilter, MSW and Cluster code merge

---
+++ rWorkspace/msw_cluster/MswAlgorithm.R
@@ -0,0 +1,72 @@
+library(dplyr)
+library(tm)
+library(NLP)
+library(textreuse)
+CalculateMSWMatrix <-  function(input_file_path ,
+                                msw_text_result_path,
+                                similarity_index_path,
+                                source_of_other_files_path   ) {
+  source(paste0(source_of_other_files_path,'/',"logger.R"))
+  setwd(input_file_path)
+  filenames <- list.files(input_file_path, pattern = '*.csv')
+  len = length(filenames)
+  
+  for (i in 1:len) {
+    f1 = filenames[i]
+    data_set <- try(read.csv(f1, na.strings = c("", "NA")),silent = TRUE)
+    if (inherits(data_set1, "try-error"))
+    {
+      message_string = paste("Error 1: Unable to read file")
+      logEvent(message_string, "Error")
+      print (message_string)
+      return (FALSE)
+    }
+    data_set = data_set %>% na.omit()
+    alarm_seq = paste0(data_set$Alarm.Tag, data_set$Alarm.Id)
+    # Writing csv data to text files
+    write.table(
+      alarm_seq,
+      file = paste0(msw_text_result_path, "/", "Flood", i, ".txt"),
+      quote = FALSE,
+      row.names = FALSE,
+      col.names = FALSE
+    )
+  }
+  
+  #similarity matrix calculation
+  dir <- Corpus(DirSource(msw_text_result_path))
+  #need to check for best minhash value
+  minhash <- minhash_generator(10, seed = 235)
+  ats <-
+    TextReuseCorpus(dir = msw_text_result_path, minhash_func = minhash)
+  buckets <- lsh(ats, bands = 10, progress = FALSE)
+  candidates <- lsh_candidates(buckets)
+  scores <-
+    lsh_compare(candidates, ats, jaccard_similarity, progress = FALSE)
+  #creating the matrix
+  scores_frame = as.data.frame(scores)
+  scores_matrix = as.data.frame.matrix(scores_frame)
+  #need to check,which method will be perfect here
+  d <- dist(scores_matrix, method = "euclidean")
+  #saving similarity matrix in csv
+  setwd(similarity_index_path)
+  write.csv(
+    scores,
+    file = paste0("result", ".csv"),
+    row.names = FALSE,
+    quote = FALSE
+  )
+  #Agglomerative Hierarchical Clustering
+  #need to check,which method will be perfect here
+  hc1 <- agnes(scores_frame, method = "complete")
+  hc_frame=as.data.frame(hc1)
+  hc_frame
+}
+
+
+CalculateMSWMatrix(
+  "C:/Users/Khushboo/Documents/msw_cluster/msw_csvdataset",
+  "C:/Users/Khushboo/Documents/msw_cluster/msw_textdataset",
+  "C:/Users/Khushboo/Documents/msw_cluster/similarityindex_matrix",
+  "C:/Users/Khushboo/Documents/prediction"
+)
---
+++ rWorkspace/prefilter/Prefilter.R
@@ -0,0 +1,97 @@
+PrefilterFunc <-
+  function(input_file_path ,
+           prefilter_result_path,
+           msw_result_path,source_of_other_files_path) {
+    source(paste0(source_of_other_files_path,'/',"logger.R"))
+    #assuming a thresold value
+    thresold = 0.6
+    setwd(input_file_path)
+    
+    #List csv files of the directory
+    filenames <- list.files(input_file_path, pattern = '*.csv')
+    len = length(filenames)
+    
+    #process each file of the list
+    for (i in 1:len) {
+      j = i + 1
+      while (j <= len) {
+        if (i != j) {
+          setwd(input_file_path)
+          f1 = filenames[i]
+          f2 = filenames[j]
+          
+          #calculating prefilter similarity index for pair of files at once
+          data_set1 <- try(read.csv(f1, na.strings = c("", "NA")), silent = TRUE)
+          if (inherits(data_set1, "try-error"))
+          {
+            message_string = paste("Error 1: Unable to read file")
+            logEvent(message_string, "Error")
+            print (message_string)
+            return (FALSE)
+          }
+          data_set1 = data_set1%>%na.omit()
+          nrow_f1 = nrow(data_set1)
+          data_set2 <-try(read.csv(f2, na.strings = c("", "NA")), silent = TRUE)
+          if (inherits(data_set2, "try-error"))
+          {
+            message_string = paste("Error 1: Unable to read file")
+            logEvent(message_string, "Error")
+            print (message_string)
+            return (FALSE)
+          }
+          data_set2 = data_set2%>%na.omit()
+          nrow_f2 = nrow(data_set2)
+          max_nrow = max(nrow_f1, nrow_f2)
+          #Check for id column name
+          alarm_seq1 = paste0(data_set1$Alarm.Tag, data_set1$Alarm.Id)
+          alarm_seq2 = paste0(data_set2$Alarm.Tag, data_set2$Alarm.Id)
+          common_seq = intersect(alarm_seq1, alarm_seq2)
+          len1 = length(common_seq)
+          si = len1 / max_nrow
+          if (si >= thresold)
+          {
+            #saving a file to a particular directory
+            setwd(prefilter_result_path)
+            write.csv(
+              data_set1,
+              file = paste0(f1),
+              row.names = FALSE,
+              quote = FALSE
+            )
+            write.csv(
+              data_set2,
+              file = paste0(f2),
+              row.names = FALSE,
+              quote = FALSE
+            )
+          }
+          else
+          {
+            setwd(msw_result_path)
+            write.csv(
+              data_set1,
+              file = paste0(f1),
+              row.names = FALSE,
+              quote = FALSE
+            )
+            write.csv(
+              data_set2,
+              file = paste0(f2),
+              row.names = FALSE,
+              quote = FALSE
+            )
+            
+          }
+        }
+        j = j + 1
+      }
+    }
+    
+  }
+
+PrefilterFunc(
+  "C:/Users/Khushboo/Documents/prefilter/alarm_dataset",
+  "C:/Users/Khushboo/Documents/prefilter/prefilter_results",
+  "C:/Users/Khushboo/Documents/msw_cluster/msw_csvdataset",
+  "C:/Users/Khushboo/Documents/prediction"
+)
