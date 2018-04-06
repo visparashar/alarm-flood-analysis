ce8bfe2bcb71b92043961aab864a0f37558e5e45
Vishal <vis.parashar@gmail.com>
2018-04-06 15:53:32 +0530

    changes for e2e vishal

--- rWorkspace/msw_cluster/MswAlgorithm.R
+++ rWorkspace/msw_cluster/MswAlgorithm.R
@@ -5,8 +5,8 @@ library(textreuse)
 library(cluster)
 CalculateMSWMatrix <-  function(input_file_path ,
                                 msw_text_result_path,
-                                similarity_index_path,
-                                source_of_other_files_path   ) {
+                                similarity_index_path
+                                   ) {
   # source(paste0(source_of_other_files_path,'/',"logger.R"))
   setwd(input_file_path)
   filenames <- list.files(input_file_path, pattern = '*.csv')
@@ -15,18 +15,16 @@ CalculateMSWMatrix <-  function(input_file_path ,
   for (i in 1:len) {
     f1 = filenames[i]
     data_set <- try(read.csv(f1, na.strings = c("", "NA")),silent = TRUE)
-    if (inherits(data_set1, "try-error"))
-    {
-      message_string = paste("Error 1: Unable to read file")
-      logEvent(message_string, "Error")
-      print (message_string)
-      return (FALSE)
-    }
+    # if (inherits(data_set, "try-error"))
+    # {
+    #   message_string = paste("Error 1: Unable to read file")
+    #   logEvent(message_string, "Error")
+    #   print (message_string)
+    #   return (FALSE)
+    # }
     data_set = data_set %>% na.omit()
     alarm_seq = paste0(data_set$Alarm.Tag, data_set$Alarm.Id)
-	#writing data to csv for knn calculation
-    test_set =cbind(Alarm_Seq=alarm_seq ,FloodNum=i)
-    write.csv(test_set,file = paste0(knn_path, "/", "Flood_Type", i,".csv"),row.names = FALSE,quote = FALSE)
+	
     # Writing csv data to text files
     write.table(
       alarm_seq,
@@ -40,7 +38,7 @@ CalculateMSWMatrix <-  function(input_file_path ,
   #similarity matrix calculation
   dir <- Corpus(DirSource(msw_text_result_path))
   corpus <-
-    TextReuseCorpus(dir = msw_text_result_path, minhash_func = minhash)
+    TextReuseCorpus(dir = msw_text_result_path)
   #pairwise comparison method
   scores <- pairwise_compare(corpus,jaccard_similarity)
   scores_pair=pairwise_candidates(scores)
@@ -56,12 +54,13 @@ CalculateMSWMatrix <-  function(input_file_path ,
     row.names = FALSE,
     quote = FALSE
   )
+  return(scores_frame)
 }
 
 
-CalculateMSWMatrix(
- "C:/Workspace_alarmflood/alarm-food-analysis/rWorkspace/msw_cluster/msw_csvdataset",
- "C:/Workspace_alarmflood/alarm-food-analysis/rWorkspace/msw_cluster/msw_textdataset",
- "C:/Workspace_alarmflood/alarm-food-analysis/rWorkspace/msw_cluster/similarityindex_matrix",
- ""
-)
+#CalculateMSWMatrix(
+# "C:/Workspace_alarmflood/alarm-food-analysis/rWorkspace/msw_cluster/msw_csvdataset",
+# "C:/Workspace_alarmflood/alarm-food-analysis/rWorkspace/msw_cluster/msw_textdataset",
+# "C:/Workspace_alarmflood/alarm-food-analysis/rWorkspace/msw_cluster/similarityindex_matrix",
+# ""
+#)
