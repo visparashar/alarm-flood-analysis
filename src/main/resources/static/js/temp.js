fee7a7d88df82325607a0341e3507230e79989b5
Z003W8WA <Z003W8WA@AAEINGGN00311D.ad001.siemens.net>
2018-04-16 10:54:11 +0530

    dd

--- src/main/resources/static/js/temp.js
+++ src/main/resources/static/js/temp.js
@@ -1,83 +1,73 @@
 function processData(allRows) {
-       var ss = allRows.split(",");
-       var x1 = [];
-       var y1 = [];
-       var z1 = [];
-       var i;
-       var j;
-       var k;
-       var f=1;
-       var n=0;
-       var s=0;
-       var z=1;
-       var t=0;
-       for (i = 0; i < ss.length; i++) {
-              var eachRow = ss[i].split("#~#");
-              if(i==0){
-                     y1.push(eachRow[0]);
-              }
-              if (x1.indexOf(eachRow[0]) == -1){
-                     x1.push(eachRow[0]);
-              }
-              if(i==ss.length-1){
-                     x1.push(eachRow[1]);
-              }
-              if (y1.indexOf(eachRow[1]) == -1){
-                     y1.push(eachRow[1]);
-              }
-       }
-       var tempArr = [];
-       tempArr[0]=1;
-       var temp=ss[0].split("#~#")[0];
-       for (j = 0; j < ss.length; j++) {
-              var row = ss[j].split("#~#");
-              if (temp == row[0]) {
-                     tempArr[f] = row[2];
-                     f=f+1;
-              }
-              else {
-                     z1[n] = tempArr;
-                     alert(tempArr);
-                     n=n+1;
-                     temp = row[0];
-                     f=1;
-                     tempArr = [];
-                     
-                     for(var g=0;g<z;g++){
-                           temp[g]=z1[g][g+1+t];
-                           
-                     }
-                     t=t+1;
-                     temp[z]=1;
-                     z=z+1;
-                     
-                     
-                     tempArr[0]=1;
-                     tempArr[f] = row[2];
-                     f=2;
-              }
-              if(j==ss.length-1){
-                     z1[n] = tempArr;
-                     n=n+1;
-                     f=1;
-                     tempArr = [];
-                     tempArr[0]=1;
-                     z1[n] = tempArr;
-              }
+	var ss = allRows.split(",");
+	var x1 = [];
+	var y1 = [];
+	var z1 = [];
 
-       }
-       var data = [ {
-              z : z1,
-              x : x1,
-              y : y1,
-              type : 'heatmap'
-       } ];
-       var layout = {
-                     title : 'Similarity Matrix',
-                     showlegend : true
-       };
+	for (var i = 0; i < ss.length; i++) {
+		var eachRow = ss[i].split("#~#");
+		if(i==0){
+			y1.push(eachRow[0]);
+			x1.push(eachRow[0]);
+		}
+		if (x1.indexOf(eachRow[0]) == -1){
+			if((y1.indexOf(eachRow[0]) > -1)
+			y1.push(eachRow[0]);
+			if((x1.indexOf(eachRow[0]) > -1)
+			x1.push(eachRow[0]);
+		}
+		if(i==ss.length-1){
+			if((y1.indexOf(eachRow[1]) > -1)
+			y1.push(eachRow[1]);
+			if((x1.indexOf(eachRow[1]) > -1)
+			x1.push(eachRow[1]);
+		}
+		if (y1.indexOf(eachRow[1]) == -1){
+			if((y1.indexOf(eachRow[1]) > -1)
+			y1.push(eachRow[1]);
+			if((x1.indexOf(eachRow[1]) > -1)
+			x1.push(eachRow[1]);
+		}
+	}
+	var tempArr = [];
+	var n=0;
+	var m=0;
+	var temp=ss[0].split("#~#")[0];
+	for(var u=0;u<x1.length;u++){
+		for(var v=0;u<y1.length;v++){
+			for(var w = 0; w < ss.length; w++){
+				var row = ss[w].split("#~#");
+				if(x1[u]==y1[u]){
+					tempArr[n]=1;
+					n=n+1;
+				}
+				else{((x1[u]==row[0] & y1[v]==row[1]) | (x1[u]==row[1] & y1[v]==row[0])){
+					if()
+					tempArr[n]=row[2];
+					n=n+1;
+				}
+				}
+				
+			}
+		}
+		z1[m]= tempArr;
+		m=m+1;
+		tempArr = [];
+		n=0;
+	}
+	
+	var data = [ {
+		z : z1,
+		x : x1,
+		y : y1,
+		type : 'heatmap'
+	} ];
+	var layout = {
+			title : 'Similarity Matrix',
+			showlegend : true
+	};
 
-       Plotly.newPlot('plotsimilarity', data, layout, {
-              displayModeBar : false
-       });
-}
+	Plotly.newPlot('plotsimilarity', data, layout, {
+		displayModeBar : false
+	});
+} 
